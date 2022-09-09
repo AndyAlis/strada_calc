1bf062861f539c873a763f24517d47758e02512e
AndyAlis <lisligi@mail.ru>
2022-09-09 20:49:46 +0300

    add saved results to the page

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
 let btn = document.getElementById("btn");
-let result = document.getElementById("result");
+let resultSpan = document.getElementById("result");
+let results = document.querySelector(".results");
+
+
+
+function calc(id, a, b) {
+    switch(id) {
+        case "+": return a + b;
+        case "*": return a * b;
+        case "-": return a - b;
+        case "/": return a / b;
+        // default : return "я не знаю такой операции " + id;
+    }
+}
+
 btn.addEventListener("click", () => {
-    // console.log("нажал!");
-    result.innerText = "зачем нажал?";
+    let a = document.getElementById("firstNumber").value;
+    let b = document.getElementById("secondNumber").value;
+    let id = document.getElementById("operations").value;
+    
+    if((a === "")||(b === "")) {
+        alert("Введите или выберете значения!")
+    }
+    else {
+        let result = Number((calc(id, Number(a), Number(b)).toFixed(10)));
+        resultSpan.innerText = result;
+        let span = document.createElement('span');
+        span.classList.add("results__span");
+        span.innerText = result;
+        span.addEventListener ("click", () => {
+            span.remove();
+        });
+        results.append(span);
+
+    };
+
+    
 });
+
